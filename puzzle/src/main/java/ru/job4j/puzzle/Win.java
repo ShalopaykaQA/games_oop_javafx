13b73911c578c50ea0934040f3fcdd1ccb8846b7
Elvira Semenova <shalopayka@gmail.com>
2020-12-04 12:35:55 +0300

    Test task

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,36 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
+        boolean rsl = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1) {
+                if ((Win.checkHorizontal(board, i)) || (Win.checkVertical(board, i))) {
+                    rsl = true;
+                    break;
+                }
+            }
+        }
         return rsl;
     }
+
+    public static boolean checkHorizontal(int[][] board, int row) {
+        boolean rsl = true;
+        for (int column = 0; column < board.length; column++) {
+            if (board[row][column] != 1) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+        public static boolean checkVertical(int[][] board, int column) {
+            boolean result = true;
+            for (int row = 0; row < board.length; row++) {
+                if (board[row][column] != 1) {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
 }
